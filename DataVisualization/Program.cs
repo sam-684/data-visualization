066a9388a919f7a4484d4ebd91836ea4bf9f2978
Shailesh <shailesh@Shaileshs-MacBook-Air.local>
2023-09-17 17:26:31 +0530

    fixed: formating of code

--- DataVisualization/Program.cs
+++ DataVisualization/Program.cs
@@ -2,20 +2,20 @@
 
 namespace DataVisualization;
 
-    public class Program
+public class Program
+{
+    public static async Task Main()
     {
-        public static async Task Main()
+        var productData = new ProductData();
+        var productList = await productData.GetCommonProductListAsync();
+
+        var productTable = new ConsoleTable("ItemCode", "UPC");
+
+        foreach (var product in productList)
         {
-            var productData = new ProductData();
-            var productList = await productData.GetCommonProductListAsync();
-
-            var productTable = new ConsoleTable("ItemCode", "UPC");
-
-            foreach (var product in productList)
-            {
-                productTable.AddRow(product.itemCode, product.upc);
-            }
-
-            Console.WriteLine(productTable);
+            productTable.AddRow(product.itemCode, product.upc);
         }
+
+        Console.WriteLine(productTable);
     }
+}
